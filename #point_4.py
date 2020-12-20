c00119c838497161394eaaa0db89fd22b581488d
fitkhullov <anton.fitkhullov@gmail.com>
2020-12-21 01:56:14 +0700

    Задания №2-6 из ДЗ №1

--- #point_4.py
+++ #point_4.py
@@ -4,15 +4,12 @@ l = len(N)
 N = int(N)
 f_dig = N//10**(l-1)
 s_dig = (N - f_dig*10**(l-1))//10**(l-2)
-if (f_dig < s_dig): max_dig = s_dig
-else: max_dig = f_dig
+max_dig = s_dig if f_dig < s_dig else f_dig
 while (l>0):    
     N -= (f_dig*10**(l-1) + s_dig*10**(l-2))
     l -= 2
     f_dig = N//10**(l-1)
     s_dig = (N - f_dig*10**(l-1))//10**(l-2)
-    if (f_dig < s_dig): curr_max_dig = s_dig
-    else: curr_max_dig = f_dig
-    if (max_dig < curr_max_dig):
-        max_dig = curr_max_dig
+    curr_max = s_dig if f_dig < s_dig else f_dig
+    max_dig = curr_max if max_dig < curr_max else max_dig    
 print(max_dig)
