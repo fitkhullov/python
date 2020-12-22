4647d55f5b1bc1c1192baa0155e4aa92cbd78565
fitkhullov <anton.fitkhullov@gmail.com>
2020-12-23 04:56:07 +0700

    cut some code

--- #2_point_4.py
+++ #2_point_4.py
@@ -2,7 +2,6 @@
 string = input()
 list_of_string = string.split(' ')
 for num, word in enumerate(list_of_string):
-    if len(word) > 10:
-        print(f'{num+1}. {word[0:10]}')
-    else:
-        print(f'{num+1}. {word}')
+    print(f'{num+1}. {word[0:10]}')
+    
+       
