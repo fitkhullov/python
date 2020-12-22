f17de4a7ea71ce51a39966d9f9dcbeef86a169c4
fitkhullov <anton.fitkhullov@gmail.com>
2020-12-23 05:30:54 +0700

    Теперь, задача выполняется гарантировано за 1 проход по коллекции

--- #2_point_5.py
+++ #2_point_5.py
@@ -1,9 +1,17 @@
 #point_5
 import random
 N = 10
+k = 0 #итератор
 rate_list = [random.randint(0, 100) for i in range(N)]
 rate_list.sort(reverse = True)
 print(rate_list)
-rate_list.append(int(input('Введите новый элемент рейтинга:')))
-rate_list.sort(reverse = True)
+new_el = int(input('Введите новый элемент рейтинга:'))
+for el in rate_list:
+    if new_el < el:
+        k += 1                       
+    else:
+        rate_list.insert(k, new_el)
+        break
+if k == len(rate_list):
+    rate_list.append(new_el)
 print(rate_list)
