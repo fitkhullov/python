b570562269d252ee148986ab2be334b4fbd1b7e4
fitkhullov <74958565+fitkhullov@users.noreply.github.com>
2021-01-04 19:00:04 +0300

    Update #5_point_1.py
    edit code

--- lesson_5/#5_point_1.py
+++ lesson_5/#5_point_1.py
@@ -2,7 +2,7 @@
 with open('input_1.txt', 'a') as fin:
     while True:
         string = input()
-        if string == '':
+        if not string:
             break
         else:
             fin.write(string + '\n')
