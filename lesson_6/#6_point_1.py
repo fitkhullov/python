68256ddd0fa3de6173b0b661f4f56111637100c0
fitkhullov <anton.fitkhullov@gmail.com>
2021-01-18 15:36:40 +0800

    another exception handled

--- lesson_6/#6_point_1.py
+++ lesson_6/#6_point_1.py
@@ -5,20 +5,23 @@ from time import sleep
 class TrafficLight:
     light_order = {'Red': 7, 'Yellow': 2, 'Green': 5}
     def __init__(self):
-        self.__color = 'Red'        
+        self.__color = 'Red'
+        self.__next_color = 'Red'
     def running(self):        
         while True:
-            print(self.__color)
             curr_color = self.__color
-            next_color = 'Red' if self.__color == 'Green' else list(self.light_order)[list(self.light_order).index(self.__color) + 1]
+            if self.__color != self.__next_color:
+                return
+            print(self.__color)
+            self.__next_color = 'Red' if self.__color == 'Green' else list(self.light_order)[list(self.light_order).index(self.__color) + 1]
             for sec in range(self.light_order[self.__color]):
                 print(sec+1)
                 if curr_color != self.__color:
                     print('Я СЛОМАЛСЯ(')
                     return
                 sleep(1)
-            self.__color = 'Red' if self.__color == 'Green' else list(self.light_order)[list(self.light_order).index(self.__color) + 1]
-            if next_color != self.__color:
+            self.__color = self.__next_color
+            if self.__next_color != self.__color:
                 return
     def change_color(self, color):
         sleep(0.5)
