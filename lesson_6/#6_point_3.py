7928b73f7da283616d63a76e12ee8ff136e7aac0
fitkhullov <74958565+fitkhullov@users.noreply.github.com>
2021-01-26 16:04:45 +0800

    Update #6_point_3.py

--- lesson_6/#6_point_3.py
+++ lesson_6/#6_point_3.py
@@ -9,6 +9,7 @@ class Worker:
 class Position(Worker):
     def get_full_name(self):
         return (self.name + ' ' + self.surname)
+    
     def get_total_income(self):
         return self._income['wage'] + self._income['bonus']
 
